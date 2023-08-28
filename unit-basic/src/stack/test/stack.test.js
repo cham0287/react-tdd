c680cf9c1f9182b0b65ff5f8ecd39b3aa492d706
cham0287 <crtmt97@gmail.com>
2023-08-29 00:24:08 +0900

    Feat: stack tdd

---
+++ unit-basic/src/stack/test/stack.test.js
@@ -0,0 +1,47 @@
+const Stack = require('../stack.js');
+
+describe('Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty', () => {
+    expect(stack.size()).toBe(0);
+  });
+
+  it('allows to push item', () => {
+    stack.push('5');
+    expect(stack.size()).toBe(1);
+  });
+
+  describe('pop', () => {
+    it('throws an error if stack is empty', () => {
+      expect(() => {
+        stack.pop();
+      }).toThrow('Stack is empty');
+    });
+
+    it('returns the last pushed item and remove from the stack', () => {
+      stack.push('banana');
+      stack.push('apple');
+      expect(stack.pop()).toBe('apple');
+      expect(stack.size()).toBe(1);
+    });
+  });
+
+  describe('peek', () => {
+    it('throws an error if stack is empty', () => {
+      expect(() => {
+        stack.peek();
+      }).toThrow('Stack is empty');
+    });
+    it('returns the last pushed item and remove from the stack', () => {
+      stack.push('banana');
+      stack.push('apple');
+      expect(stack.peek()).toBe('apple');
+      expect(stack.size()).toBe(2);
+    });
+  });
+});
